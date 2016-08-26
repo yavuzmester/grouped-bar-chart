f82af840816ec6079c9b1e81fc89ff208c3cb14d
gokhanayhan38 <789456123aA>
2016-08-26 09:36:00 +0300

    build

---
+++ demo/app.js
@@ -0,0 +1,31 @@
+const React = require("react");
+const ReactDOM = require("react-dom");
+const GroupedBarChartSvg = require("grouped-bar-chart-svg");
+
+GBCSProps = {
+    "title": "bearercode",
+    "svgMargin": {"left":110,"right":50,"top":20,"bottom":30},
+    svgWidth: 200,
+    divWidth: 360,
+    "data": [{
+        "category": "2G",
+        "value": 1690,
+        "groupId": "62"
+    }, {
+        "category": "3G",
+        "value": 2607,
+        "groupId": "62"
+    }],
+    "categoriesSize": 2,
+    "groups": [{
+        "groupId": "62",
+        "groupColor": "#E41A1C"
+    }],
+    "logaxis": false,
+    "selection": ["2G", "3G"]
+};
+
+setTimeout(() => {
+    const gbc = ReactDOM.render(React.createElement(GroupedBarChartSvg, GBCSProps), document.getElementById("root"));
+    gbc.on("title-click", () => console.log("title-click"));
+}, 100);
--- test/app.js
+++
@@ -1,7 +0,0 @@
-const React = require("react");
-const ReactDOM = require("react-dom");
-const GroupedBarChartSvg = require("grouped-bar-chart-svg");
-
-setTimeout(() => {
-    ReactDOM.render(React.createElement(GroupedBarChartSvg), document.getElementById("root"));
-}, 100);
